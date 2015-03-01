506bc23121fe438a87a65f8ecd2897553fa3e9b9
koskot77 <koskot77@hotmail.com>
2015-03-01 20:37:50 +0100

    wrapping up

--- toolbox.h
+++ toolbox.h
@@ -6,17 +6,17 @@
 #define MAX_ADAPTORS (128)
 #define MAX_LENGTH   (128)
 
-// A colisionless hash-like helper function to convert a sequence of bases (limited to 32 symbols) into an integer number 
-//   in case of a problem (e.g. uninterpretable sequence) last arguments returns position of the error (starting from 1)
+// A collisions hash-like helper function to convert a sequence of bases (limited to 32 symbols) into an integer number 
+//   in case of a problem (e.g. non-interpretable sequence) last arguments returns position of the error (starting from 1)
 unsigned long long sequence2number(const char *sequence, unsigned short length, unsigned short &errPos){
     if( length > 32 ) return 0;
     // ascii codes of the symbols:
     //  T:  84, G: 71, A:65, C:67
     //  t: 116, g:103, a:97, c:99
-    // construct the look-up table symbolic code -> a digit
+    // construct a look-up table: symbolic code -> digit
     // reserve code '4' to find out if the sequence has anything else besides precisely the four expected bases (i.e. T,G,A,C)
     const static unsigned short ascii2digit[128] = {
-        // assign each symbol with an unique number from 0 to 3
+        // assign each symbol with an unique number from 0 to 4
         #define T 0
         #define G 1
         #define A 2
@@ -84,7 +84,7 @@ private:
     unsigned long long *data;   // numeric representation of the sequence
     size_t size, length;        // size of the array and length of the original symbolic sequence
     const static unsigned symbolsInOneElement; // number of symbols coded by a single element of the numeric array
-    unsigned long errorPos;    // first occurance position of an interpretation problem (if any -> starting from 1)
+    unsigned long errorPos;    // first occurrence position of an interpretation problem (if any -> starting from 1)
 
 public:
     unsigned long error(void) const { return errorPos; }
@@ -102,13 +102,14 @@ public:
         retval = data[block] >> (index*2);
         // see if we need to take the rest from next element
         if( nLeft>0 )
-            retval |= (data[block+1]&(((unsigned long long)(0x1)<<(nLeft*2))-1)) << ((symbolsInOneElement-index)*2);
+            retval |= (data[block+1]&((0x1LL<<(nLeft*2))-1)) << ((symbolsInOneElement-index)*2);
         else
-            retval &= ((unsigned long long)(0x1)<<(width*2)) - 1;
+            retval &= (0x1LL<<(width*2)) - 1;
 
         return retval;
     }
 
+    // construct numeric sequence from a symbolic sequence
     NumericSequence& operator=(const char *symbolicSequence){
         // reset errors
         unsigned short err = 0;
@@ -157,7 +158,7 @@ const unsigned NumericSequence::symbolsInOneElement = sizeof(unsigned long long)
 #define MAX_COLLISIONS (10)   // allow up to 10 collisions
 class LookUpTable {
 private:
-    unsigned long long values [MAX_ADAPTORS+1];  // original values; indexing starts from 1 
+    unsigned long long values [MAX_ADAPTORS+1];  // original values (converted keys); indexing starts from 1 
     size_t             lengths[MAX_ADAPTORS+1];  // we should also carry around the length of original sequence in case it was ending with 'T's
     size_t **table; //[BUCKETS][MAX_COLLISIONS]; // look-up table; stores 0 for empty buckets or an index of a potential match for a given hash value
     size_t  *nCollisions; //[BUCKETS];           // number of collisions (should mostly be 0)
@@ -170,8 +171,8 @@ public:
         return retval;
     }
 
-    // quick search function returns MAX_ADAPTORS if requested number is not among the original values
-    size_t find(unsigned long long number, size_t length) const {
+    // quick search function returns MAX_ADAPTORS if requested number is not among the known keys, otherwise key index (start from 0)
+    size_t find(unsigned long long number, size_t &length) const {
         // following two lines is as far as we get most of the times
         size_t bucket = number % BUCKETS;
         if( table[bucket][0] == 0 ) return MAX_ADAPTORS;
@@ -179,21 +180,42 @@ public:
         // or we may have found a potential match; check if it indeed the case
         size_t matchIndex = MAX_ADAPTORS;
 
-        for(size_t col=0; col <= nCollisions[bucket] && matchIndex == MAX_ADAPTORS; col++)
-            if( number == values [ table[bucket][col] ] &&
-                length == lengths[ table[bucket][col] ] )
-                matchIndex = table[bucket][col];
+        // look for complete or partial match
+        size_t bestMatchLength = 0;
+        for(size_t col=0; col <= nCollisions[bucket] && matchIndex == MAX_ADAPTORS; col++){
+            size_t             ind = table[bucket][col];
+            unsigned long long val = values [ind];
+            size_t             len = lengths[ind];
+            if( (number & ((0x1LL<<(len*2))-1)) == val ){
+                bestMatchLength = len;
+                matchIndex = ind;
+            }
+        }
 
         // if match is found make the index starting from 0
         if( matchIndex != MAX_ADAPTORS ) matchIndex--; 
 
+        length = bestMatchLength;
+
         return matchIndex;
     }
 
+    // construct the table from the set of keys (key's index serves the value)
     LookUpTable(const char keys[MAX_ADAPTORS][MAX_LENGTH]){
+        // keys may have arbitrary lengths, but the quick search function operates on the fixed length numbers
+        //  this is achieved with padding of the shorter keys to the maximal key length
+        //  in the process the shorter keys are multiplexed to all possible combinations of padding symbols
+
         // initialize static members
         bzero(values,  sizeof(values));
         bzero(lengths, sizeof(lengths));
+
+        // find longest key
+        size_t maxKeyLength = 0;
+        for(size_t k=0; k<MAX_ADAPTORS; k++){
+            size_t length = strlen( keys[k] );
+            if( length > maxKeyLength ) maxKeyLength = length;
+        }
 
         // allocate and initialize dynamic tables
         nCollisions = new size_t [BUCKETS];
@@ -205,18 +227,27 @@ public:
             bzero(table[k], sizeof(size_t)*MAX_COLLISIONS);
         }
 
-        // Build hash values for the adaptors
+        // build hash values for the padded keys
         for(size_t k=0; k<MAX_ADAPTORS; k++){
             size_t length = strlen( keys[k] );
             if( length > 0 ){
-                lengths[k+1] = length;
                 unsigned short errorPos = 0;
-                values [k+1] = sequence2number(keys[k],length,errorPos);
-                size_t bucket = values[k+1] % BUCKETS;
-                size_t collision = 0;
-                while( table[bucket][collision] ) collision++;
-                table[bucket][collision] = k+1;
-                nCollisions[bucket] = collision;
+                unsigned long long coreCode = sequence2number(keys[k],length,errorPos);
+                // skip any non-interpretable key
+                if( errorPos ) continue;
+                // remember the sequence in numeric form
+                lengths[k+1] = length;
+                values [k+1] = coreCode; 
+
+                // now create all new padding combinations and add those to the table
+                for(size_t padCode=0; padCode<(0x1<<((maxKeyLength - length)*2)); padCode++){
+                    unsigned long long code = coreCode | (padCode<<(length*2));
+                    size_t bucket = code % BUCKETS;
+                    size_t collision = 0;
+                    while( table[bucket][collision] ) collision++;
+                    table[bucket][collision] = k+1;
+                    nCollisions[bucket] = collision;
+                }
             }
         }
     }
